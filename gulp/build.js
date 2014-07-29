92aa7c265f2841785a82db213d0ee08a62df7836
Theodor Văraru <theo@vararu.org>
2014-07-29 14:32:03 +0300

    Initial browserify gulp task.

--- gulp/build.js
+++ gulp/build.js
@@ -1,8 +1,23 @@
 'use strict';
 
 var gulp = require('gulp');
+var browserify = require('gulp-browserify');
 
-gulp.task('build', function () {
+gulp.task('build', ['html', 'js', 'css'], function () {
   console.log('build task');
+});
+
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js', function () {
+  gulp.src('./app/js/main.js')
+    .pipe(browserify())
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('css', function () {
 });
 
