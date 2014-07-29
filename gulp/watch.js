f9bb15454fe3d0ec86893fbda7f08d0860cffe6b
Theodor Văraru <theo@vararu.org>
2014-07-29 16:57:12 +0300

    Rename browser-sync task to serve.

--- gulp/watch.js
+++ gulp/watch.js
@@ -3,20 +3,20 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
-gulp.task('browser-sync', function () {
-  browserSync({
-    server: {
-      baseDir: './dist'
-    }
-  });
-});
-
-gulp.task('watch', ['build', 'browser-sync'], function () {
+gulp.task('watch', ['build', 'serve'], function () {
   gulp.watch('./app/**/*.html', ['html']);
   gulp.watch('./app/**/*.js', ['js']);
 
   gulp.watch('./dist/**/*').on('change', function () {
     browserSync.reload();
+  });
+});
+
+gulp.task('serve', function () {
+  browserSync({
+    server: {
+      baseDir: './dist'
+    }
   });
 });
 
