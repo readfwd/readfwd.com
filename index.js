1ced43021c591f0e052693f73e19eee7f9971a68
Theodor Văraru <theo@vararu.org>
2014-07-31 21:29:04 +0300

    Enable mean-seo middleware in Express.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var compression = require('compression');
+var seo = require('mean-seo');
 
 var app = express();
 
 // Enable gzip compression.
 app.use(compression());
+
+// Enable PhantomJS-enabled SEO.
+app.use(seo());
 
 app.use('/', express.static(__dirname + '/dist'));
 app.listen(process.env.PORT);
