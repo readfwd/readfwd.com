ad69eeaa9fe553336e716d4bce81a9472c7a44fa
Theodor Văraru <theo@vararu.org>
2014-07-31 23:23:02 +0300

    Switch to redis based caching.

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ var app = express();
 app.use(compression());
 
 // Enable PhantomJS SEO.
-app.use(seo());
+if (process.env.REDISCLOUD_URL) {
+  // If we've got Redis available, use that.
+  app.use(seo({ cacheClient: 'redis' }));
+} else {
+  // Otherwise, use regular disk-based cache.
+  app.use(seo());
+}
 
 app.use('/', express.static(__dirname + '/dist'));
 app.listen(process.env.PORT);
