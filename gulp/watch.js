1ef7d2a06cd68bee475b0d11309c7666d0779d42
Andrei Constantinescu <andrei.c@icloud.com>
2014-08-25 13:20:41 +0300

    Modify package.json and gulp tasks

--- gulp/watch.js
+++ gulp/watch.js
@@ -7,33 +7,37 @@ var paths = config.paths;
 
 var browserSync = require('browser-sync');
 
-gulp.task('watch', ['build', 'serve'], function () {
+// Common watch hooks.
+gulp.task('watch:common', ['build'], function () {
   gulp.watch(paths.app + '/index.jade', ['index.html']);
-  gulp.watch(paths.app + '/templates/*.jade', ['jade']);
-  gulp.watch(paths.app + '/**/*.js', ['js']);
-
-  gulp.watch(paths.tmp + '/*').on('change', function () {
-    browserSync.reload();
-  });
+  gulp.watch(paths.app + '/templates/**/*.jade', ['templates']);
+  gulp.watch(paths.app + '/**/*.js', ['js:dev']);
+  gulp.watch(paths.app + '/**/**/*.styl', ['css']);
+  gulp.watch(['bower.json'], ['wiredep']);
 });
 
+// Build the project and start a web development server.
+gulp.task('watch', ['watch:common'], function (done) {
+  browserSyncRun(done, [paths.tmp, paths.app]);
+});
+
+function browserSyncRun(done, path) {
+  browserSync({
+    server: {
+      baseDir: path
+    },
+    port: 4000
+  }, function () {
+    done();
+  });
+}
+
+// Serve the ./.tmp folder using a static web development server.
 gulp.task('serve', function (done) {
-  browserSync({
-    server: {
-      baseDir: paths.tmp
-    }
-  }, function () {
-    done();
-  });
+  browserSyncRun(done, [paths.tmp, paths.app]);
 });
 
+// Serve the ./dist folder using a static web development server.
 gulp.task('serve:dist', function (done) {
-  browserSync({
-    server: {
-      baseDir: paths.dist
-    }
-  }, function () {
-    done();
-  });
+  browserSyncRun(done, paths.dist);
 });
-
